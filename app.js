ef51caded56f8216fcda994ddb4a18245be38a25
Italo Eduardo Santos Souza <italohttp25@gmail.com>
2022-10-17 17:08:55 -0300

    extrai resultado de queries de cada coluna

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mysql = require('mysql');
 const bodyParser = require("body-parser");
 
+
 app.set("view engine","ejs")
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -17,18 +18,13 @@ const connection = mysql.createConnection({
 
 
 app.get("/",function(req,res){
-	let q = 'select * from usuarios';
-	connection.query(q, function(error,results){
-		if(error) throw error;
+//	let q = 'select * from usuarios';
+//	connection.query(q, function(error,results){
+//		if(error) throw error;
 		res.render("home");
-		console.log(results[0].id,
-            results[0].email,
-            results[0].senha);
-        console.log(results[1].id,
-            results[1].email,
-            results[1].senha)
+//		console.log(results)
 	});
-});
+//});
 
 app.post("/register",function(req,res){
 	var person = {email:req.body.email,
@@ -42,14 +38,31 @@ app.post("/register",function(req,res){
 });
 
 app.get("/usuarios",function(req,res){
-	var q = 'select * from usuarios';
-	connection.query(q, function(error,results){
-        var resultados = {results}
+	connection.query('SELECT count(*) as colunas FROM information_schema.columns WHERE table_name = "usuarios"', function(error,rows,fields){
 		if(error) throw error;
+		console.log('numero de colunas :', rows[0].colunas);
+		n_colunas = rows[0].colunas;
+	});
+
+	connection.query('select count(*) as linhas from usuarios', function(error,rows,fields){
+		if(error) throw error;
+		console.log('numero de linhas :', rows[0].linhas);
+		n_linhas = rows[0].linhas;
+	});
+	connection.query('select * from usuarios', function(error,rows,fields){
+		if(error) throw error;
+		let id = rows.map(({id})=>id);
+		let email = rows.map(({email})=>email);
+		let senha = rows.map(({senha})=>senha);
+		console.log('seu resultado é :', id);
+		console.log('seu resultado é :', email);
+		console.log('seu resultado é :', senha);
+		
 		res.render("usuarios");
-		console.log(resultados)
 	});
 });
+
+
 
 app.listen(3000,function(){
 	console.log("Server is running on port 3000!");
